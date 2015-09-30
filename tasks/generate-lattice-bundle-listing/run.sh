9168e5e430de706c0feedaefcbd915a62348a926
Colin Jackson <cojackson@pivotal.io>
2015-09-30 10:35:26 -0400

    Fix: 'lattice/concourse' -> 'lattice-ci'
    [#103801388]

--- tasks/generate-lattice-bundle-listing/run.sh
+++ tasks/generate-lattice-bundle-listing/run.sh
@@ -5,7 +5,7 @@ set -ex
 export AWS_ACCESS_KEY_ID AWS_SECRET_ACCESS_KEY
 gem install bundler
 
-pushd lattice/concourse/tasks/generate-lattice-bundle-listing
+pushd lattice-ci/tasks/generate-lattice-bundle-listing
   bundle install
   bundle exec ./generate-listing.rb
 popd
