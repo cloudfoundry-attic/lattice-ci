fe5027a7f18891a86a4594dd6101b5935fd80111
Colin Jackson <cojackson@pivotal.io>
2015-10-09 14:21:51 -0500

    setting gopath in build-ltc-tar job [#104919732]
    Signed-off-by: Colin Jackson <cojackson@pivotal.io>

--- tasks/build-ltc-tar/run.sh
+++ tasks/build-ltc-tar/run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 ltc_version=$(git -C lattice-release/src/github.com/cloudfoundry-incubator/lattice/ltc describe --tags --always)
+export GOPATH=$(cd lattice-release && pwd)
 
 pushd lattice-release
   go install github.com/cloudfoundry-incubator/lattice/ltc
