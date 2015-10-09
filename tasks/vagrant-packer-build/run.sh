f4b2ecdae0f6ba3caa8dd6229ed6d7bb31aac369
Emily Casey <ecasey@pivotal.io>
2015-10-09 14:45:21 -0500

    tar box files and upload as s3 resource [#104919732]
    Signed-off-by: Colin Jackson <cojackson@pivotal.io>

--- tasks/vagrant-packer-build/run.sh
+++ tasks/vagrant-packer-build/run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+lattice_release_version=$(git describe -C ./lattice-release --tags --always)
 
 pushd ./lattice-release/vagrant
-  ./build --only=$BUILDER
+  ./build
+  tar czf "lattice-boxes-${lattice_release_version}.tgz" lattice-*.box
 popd
