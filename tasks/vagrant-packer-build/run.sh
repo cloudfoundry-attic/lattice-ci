e1e85ce4352093618e13351145f2d946d9a8bc0e
Colin Jackson <cojackson@pivotal.io>
2015-10-09 15:48:36 -0500

    Install bosh [#104919732]
    Signed-off-by: Emily Casey <ecasey@pivotal.io>

--- tasks/vagrant-packer-build/run.sh
+++ tasks/vagrant-packer-build/run.sh
@@ -2,6 +2,8 @@
 lattice_release_version=$(git -C ./lattice-release describe --tags --always)
 export GOPATH=$(cd ./lattice-release && pwd)
 
+gem install bosh_cli
+
 pushd ./lattice-release/vagrant
   ./build
   tar czf "vagrant-boxes-${lattice_release_version}.tgz" lattice-*.box
