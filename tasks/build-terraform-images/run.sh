f1cbe9afc177470d5d777b2f5cd299e232e0aa0c
Emily Casey <ecasey@pivotal.io>
2015-10-15 16:29:15 -0400

    start fixes for more lightweight git resources
    Signed-off-by: Colin Jackson <cojackson@pivotal.io>

--- tasks/build-terraform-images/run.sh
+++ tasks/build-terraform-images/run.sh
@@ -12,7 +12,7 @@ if [[ $current_ami_commit == $next_ami_commit ]]; then
   exit 0
 fi
 
-lattice-release/terraform/build -machine-readable -var "version=$next_version" | tee build.log
+terraform-image-changes/terraform/build -machine-readable -var "version=$next_version" | tee build.log
 
 tail -50 build.log | ./lattice-ci/tasks/build-terraform-images/parse-build-output.rb > ami-metadata-v${next_version}.tf.json
 echo $next_ami_commit > ami-commit-v$next_version
