023e7530d56f20d6da1bdd2f18530f62faf4f07a
Keaty Gross <kgross@pivotal.io>
2015-10-16 16:36:47 -0400

    Trim \r from end of domain variable
    [#104919732]
    
    Signed-off-by: Stephen Levine <slevine@pivotal.io>

--- tasks/deploy-vagrant-aws/run.sh
+++ tasks/deploy-vagrant-aws/run.sh
@@ -9,7 +9,7 @@ unzip lattice-bundle-ci/lattice-bundle-v*.zip
 
 pushd lattice-bundle-v*/vagrant >/dev/null
   AWS_INSTANCE_NAME=ci-vagrant-aws vagrant up --provider=aws
-  export $(vagrant ssh -c "grep '^DOMAIN=' /var/lattice/setup" 2>/dev/null)
+  export $(vagrant ssh -c "grep '^DOMAIN=' /var/lattice/setup" 2>/dev/null | tr -d '\r')
 popd >/dev/null
 
 echo $DOMAIN > domain
