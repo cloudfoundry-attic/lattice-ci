5034a8ab0d83927b09c40560accd3948afb39a8e
Stephen Levine <slevine@pivotal.io>
2015-10-16 21:54:03 -0400

    Fix terraform AMI metadata key names
    [#104919732]

--- tasks/build-bundle/run.sh
+++ tasks/build-bundle/run.sh
@@ -14,7 +14,7 @@ echo "LATTICE_TGZ_URL = \"$lattice_tgz_url\"" > $output_dir/vagrant/Vagrantfile
 sed "$box_version_filter" lattice-release/vagrant/Vagrantfile >> $output_dir/vagrant/Vagrantfile
 
 cp -r lattice-release/terraform/aws $output_dir/terraform/
-filter='{"variables": (.variables + {"lattice_tgz_url": {"default": "'"$lattice_tgz_url"'"}})}'
+filter='{"variable": (.variable + {"lattice_tgz_url": {"default": "'"$lattice_tgz_url"'"}})}'
 jq "$filter" terraform-ami-metadata/ami-metadata-v* > $output_dir/terraform/aws/lattice.tf.json
 
 zip -r ${output_dir}.zip "$output_dir"
--- tasks/build-terraform-images/parse-build-output.rb
+++ tasks/build-terraform-images/parse-build-output.rb
@@ -2,13 +2,12 @@
 
 require 'json'
 
-ami_metadata = { "variables" => {} }
-
-$stdin.readlines.each do |line|
+variables = {}
+ARGF.each_line do |line|
   row = line.chomp.split(',')
   if row[2] == "artifact" && row[4] == "id"
-    ami_metadata["variables"][row[1]] = { "defaults" => Hash[*row[5].split(/:|%\!\(PACKER_COMMA\)/)] }
+    variables[row[1]] = {default: Hash[*row[5].split(/:|%\!\(PACKER_COMMA\)/)]}
   end
 end
 
-print ami_metadata.to_json
+print {variable: variables.to_json}.to_json
