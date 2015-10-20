6b3f1ca0087ba3783a744c872579e62c7f08cf41
Colin Jackson <cojackson@pivotal.io>
2015-10-20 15:51:38 -0400

    Fix tag-release run script typos
    [#105306942]
    
    Signed-off-by: Stephen Levine <slevine@pivotal.io>

--- tasks/tag-release/run.sh
+++ tasks/tag-release/run.sh
@@ -7,12 +7,12 @@ latest_release_commit=$(git -C version-changes rev-parse -q --verify HEAD)
 latest_release_tag=$(git -C version-changes describe)
 latest_release_brand=$(cat version-changes/VERSION)
 
-[[ latest_release_tag == latest_release_brand ]] && exit 0
+[[ $latest_release_tag == $latest_release_brand ]] && exit 0
 
 if [[ $current_commit != $latest_release_commit ]]; then
   echo "ERROR: release (version-bump) commit fetched with future changes. Aborting..."
   exit 1
 fi
 
-git -C lattice-release tag -am '' $latest_release_brand
+git -C lattice-release tag -afm '' $latest_release_brand
 git -C lattice-release push -f origin "refs/tags/$latest_release_brand:refs/tags/$latest_release_brand"
