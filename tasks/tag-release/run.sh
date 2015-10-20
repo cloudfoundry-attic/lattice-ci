94d295c9a795a080bb51e3e5b87a503b68d72947
Stephen Levine <slevine@pivotal.io>
2015-10-20 15:19:27 -0400

    Tag new releases with annotated tags
    [#105306942]
    
    Signed-off-by: Colin Jackson <cojackson@pivotal.io>

---
+++ tasks/tag-release/run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -ex
+
+current_commit=$(git -C lattice-release rev-parse -q --verify HEAD)
+latest_release_commit=$(git -C version-changes rev-parse -q --verify HEAD)
+latest_release_tag=$(git -C version-changes describe)
+latest_release_brand=$(cat version-changes/VERSION)
+
+[[ latest_release_tag == latest_release_brand ]] && exit 0
+
+if [[ $current_commit != $latest_release_commit ]]; then
+  echo "ERROR: release (version-bump) commit fetched with future changes. Aborting..."
+  exit 1
+fi
+
+git tag -am '' $latest_release_brand
+git -C lattice-release push -f --tags
