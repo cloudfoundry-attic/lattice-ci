d6e1cf7aae1711ce0a89cc680fd3fcd151b616bd
Colin Jackson <colin.s.jackson90@gmail.com>
2015-10-20 03:37:24 -0400

    Actually check whether atlas boxes were uploaded
    [#106017092]

--- tasks/cleanup-incomplete-build-vagrant/run.sh
+++ tasks/cleanup-incomplete-build-vagrant/run.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+set -ex
+
 next_version=$(cat next-vagrant-box-version/number)
 
 for p in $PROVIDERS; do
   errors=$(curl https://atlas.hashicorp.com/api/v1/box/lattice/collocated/version/$next_version/provider/$p?access_token='$ATLAS_TOKEN' | jq .errors -r)
-  if [[ errors != 'null' ]]; then
+  if [[ $errors != 'null' ]]; then
     curl https://atlas.hashicorp.com/api/v1/box/lattice/collocated/version/$next_version \
       -X DELETE \
       -d access_token=$ATLAS_TOKEN
