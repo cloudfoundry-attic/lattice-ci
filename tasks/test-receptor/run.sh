3f0958a3f271cf339815529614b3372da5173a9a
Mark DeLillo <mdelillo@pivotal.io>
2015-10-22 18:00:18 -0400

    Create lattice-release-bin directory
    [#105817320]
    
    Signed-off-by: Stephen Levine <slevine@pivotal.io>

--- tasks/test-receptor/run.sh
+++ tasks/test-receptor/run.sh
@@ -9,6 +9,7 @@ pushd lattice-release >/dev/null
   go install github.com/apcera/gnatsd
 
 
+  mkdir -p lattice-release/bin
   curl -L -O "https://dl.bintray.com/mitchellh/consul/0.5.2_linux_amd64.zip"
   unzip 0.5.2_linux_amd64.zip -d lattice-release/bin
   chmod +x lattice-release/bin/consul
