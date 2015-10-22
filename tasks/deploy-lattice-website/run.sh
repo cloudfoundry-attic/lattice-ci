d25213c47a89cfe627f083bf7c89f474ee6d0d6b
Stephen Levine <slevine@pivotal.io>
2015-10-22 16:43:02 -0400

    Configure git email and name for deploy-lattice-website
    [#103740070]
    
    Signed-off-by: Mark DeLillo <mdelillo@pivotal.io>

--- tasks/deploy-lattice-website/run.sh
+++ tasks/deploy-lattice-website/run.sh
@@ -8,6 +8,9 @@ export GIT_SSH_COMMAND="/usr/bin/ssh -i $PWD/github.key"
 
 set -x
 
+git config --global user.email "pivotal-lattice-eng@pivotal.io"
+git config --global user.name "Concourse CI"
+
 pushd lattice-website >/dev/null
   bundle install
   apt-get install node -y --no-install-recommends
