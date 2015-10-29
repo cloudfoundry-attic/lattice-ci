e4887d6f492e09d9fcbb76c40baedd25b464c84a
Stephen Levine <slevine@pivotal.io>
2015-10-29 11:46:32 -0400

    Separate deploy-bosh and apply-cloudformation tasks
    [#104962476]

---
+++ tasks/apply-cloudformation/run.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+set -ex
+
+TEMPLATE_PATH=$PWD/lattice-ci/tasks/apply-cloudformation/cloudformation.json
+
+if aws cloudformation describe-stacks --stack-name "$CLOUDFORMATION_STACK_NAME" >/dev/null 2>/dev/null; then
+  aws cloudformation update-stack --stack-name "$CLOUDFORMATION_STACK_NAME" --template-body "file://$TEMPLATE_PATH" || true
+else
+  aws cloudformation create-stack --stack-name "$CLOUDFORMATION_STACK_NAME" --template-body "file://$TEMPLATE_PATH"
+fi
+
+while true; do
+  STACK_INFO=$(aws cloudformation describe-stacks --stack-name "$CLOUDFORMATION_STACK_NAME")
+  STATUS=$(echo "$STACK_INFO" | jq -r .Stacks[0].StackStatus)
+
+  if [[ ${PIPESTATUS[0]} != 0 ]]; then
+    echo "Failed to describe stacks."
+    exit 1
+  fi
+
+  if [[ $STATUS = "ROLLBACK_COMPLETE" ]]; then
+    echo "Create failed."
+    exit 1
+  fi
+
+  if [[ $STATUS = "UPDATE_ROLLBACK_COMPLETE" ]]; then
+    echo "Update failed."
+    exit 1
+  fi
+
+  if [[ $STATUS = "CREATE_COMPLETE" ]] ; then
+    echo "$STATUS"
+    break
+  fi
+
+  sleep 5
+done
--- tasks/deploy-bosh/run.sh
+++ tasks/deploy-bosh/run.sh
@@ -6,40 +6,7 @@ echo "$BOSH_PRIVATE_KEY" > bosh.pem
 
 set -x
 
-TEMPLATE_PATH=$PWD/lattice-ci/tasks/deploy-bosh/cloudformation.json
-
-if aws cloudformation describe-stacks --stack-name "$CLOUDFORMATION_STACK_NAME" >/dev/null 2>/dev/null; then
-  aws cloudformation update-stack --stack-name "$CLOUDFORMATION_STACK_NAME" --template-body "file://$TEMPLATE_PATH" || true
-else
-  aws cloudformation create-stack --stack-name "$CLOUDFORMATION_STACK_NAME" --template-body "file://$TEMPLATE_PATH"
-fi
-
-while true; do
-  STACK_INFO=$(aws cloudformation describe-stacks --stack-name "$CLOUDFORMATION_STACK_NAME")
-  STATUS=$(echo "$STACK_INFO" | jq -r .Stacks[0].StackStatus)
-
-  if [[ ${PIPESTATUS[0]} != 0 ]]; then
-    echo "Failed to describe stacks."
-    exit 1
-  fi
-
-  if [[ $STATUS = "ROLLBACK_COMPLETE" ]]; then
-    echo "Create failed."
-    exit 1
-  fi
-
-  if [[ $STATUS = "UPDATE_ROLLBACK_COMPLETE" ]]; then
-    echo "Update failed."
-    exit 1
-  fi
-
-  if [[ $STATUS = "CREATE_COMPLETE" ]] ; then
-    echo "$STATUS"
-    break
-  fi
-
-  sleep 5
-done
+STACK_INFO=$(aws cloudformation describe-stacks --stack-name "$CLOUDFORMATION_STACK_NAME")
 
 function get_stack_output() {
   echo "$STACK_INFO" | jq -r "[ .Stacks[0].Outputs[] | { (.OutputKey): .OutputValue } | .$1 ] | add"
