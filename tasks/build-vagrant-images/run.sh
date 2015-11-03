a2ce76e33c3908bf961ecf9676a32ccb75f5f1de
Stephen Levine <slevine@pivotal.io>
2015-11-03 09:44:50 -0500

    Make build-vagrant-images use vsphere worker
    [#107073856]
    
    Signed-off-by: Colin Jackson <cojackson@pivotal.io>

--- tasks/build-vagrant-images/run.sh
+++ tasks/build-vagrant-images/run.sh
@@ -1,38 +1,75 @@
 #!/bin/bash
 
-set -ex
+set -e
 
-cd build-vagrant-prepare
-
-! [[ -f should_build ]] && exit 0
+echo "$GITHUB_SSH_KEY" > github_private_key.pem
+chmod 0600 github_private_key.pem
+eval $(ssh-agent)
+ssh-add github_private_key.pem > /dev/null
+rm github_private_key.pem
 
 current_version=$(cat current-vagrant-box-version/number)
 next_version=$(cat next-vagrant-box-version/number)
+current_box_commit=$(cat "vagrant-box-commit/box-commit-v$current_version")
+next_box_commit=$(git -C vagrant-image-changes rev-parse -q --verify HEAD)
 
-if [[ $REMOTE_EXECUTOR_IP == "no-executor" ]]; then
-  vagrant-image-changes/vagrant/build -var "version=$next_version" -only=$NAMES
-
-  cd ..
-  rm -rf build-vagrant-prepare
+if [[ $current_box_commit == $next_box_commit ]]; then
+  echo -n $current_box_commit > "box-commit-v$current_version"
+  echo -n $current_version > box-version-number
   exit 0
 fi
 
-mkdir -p $HOME/.ssh/
+git -C vagrant-image-changes submodule update --init --recursive
 
-remote_tmp="/tmp/build-vagrant-images-$(date "+%Y-%m-%d-%H%M%S")"
-ssh-keyscan -p 22222 $REMOTE_EXECUTOR_IP >> $HOME/.ssh/known_hosts
-ssh -i aws_private_key.pem pivotal@$REMOTE_EXECUTOR_IP -p 22222 mkdir -p $remote_tmp
+lattice_json=$(cat vagrant-image-changes/vagrant/lattice.json)
+post_processor_json=`
+cat <<EOF
+{
+  "post-processors": [[
+    {
+      "type": "vagrant"
+    },
+    {
+      "type": "atlas",
+      "only": ["amazon-ebs"],
+      "token": "$ATLAS_TOKEN",
+      "artifact": "lattice/colocated",
+      "artifact_type": "vagrant.box",
+      "metadata": {
+        "provider": "aws",
+        "version": "$next_version"
+      }
+    },
+    {
+      "type": "atlas",
+      "only": ["vmware-iso"],
+      "token": "$ATLAS_TOKEN",
+      "artifact": "lattice/colocated",
+      "artifact_type": "vagrant.box",
+      "metadata": {
+        "provider": "vmware_desktop",
+        "version": "$next_version"
+      }
+    },
+    {
+      "type": "atlas",
+      "only": ["virtualbox-iso"],
+      "token": "$ATLAS_TOKEN",
+      "artifact": "lattice/colocated",
+      "artifact_type": "vagrant.box",
+      "metadata": {
+        "provider": "virtualbox",
+        "version": "$next_version"
+      }
+    }
+  ]]
+}
+EOF
+`
 
-rsync -a -e "ssh -p 22222 -i aws_private_key.pem" * pivotal@$REMOTE_EXECUTOR_IP:$remote_tmp
+echo $lattice_json | jq '. + '"$post_processor_json" > vagrant-image-changes/vagrant/lattice.json
 
-ssh -i aws_private_key.pem pivotal@$REMOTE_EXECUTOR_IP -p 22222 <<ENDSSH
-export PATH=~/.rbenv/shims:/usr/local/go/bin:~/packer:/usr/local/bin:\$PATH
-export ATLAS_TOKEN=$ATLAS_TOKEN
-cd $remote_tmp
-rbenv local 2.2.3
 vagrant-image-changes/vagrant/build -var "version=$next_version" -only=$NAMES
-ENDSSH
 
-ssh -i aws_private_key.pem pivotal@$REMOTE_EXECUTOR_IP -p 22222 rm -rf $remote_tmp
-cd ..
-rm -rf build-vagrant-prepare
+echo -n $next_box_commit > "box-commit-v$next_version"
+echo -n $next_version > box-version-number
