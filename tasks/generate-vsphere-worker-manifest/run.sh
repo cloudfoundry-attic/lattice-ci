fb2b83d3fe7b73017b2a28df01b0fc21d028b0dd
Stephen Levine <slevine@pivotal.io>
2015-11-02 23:16:35 -0500

    Add BOSH director UUID to vsphere worker deployment manifest
    [#107073856]

--- tasks/generate-vsphere-worker-manifest/run.sh
+++ tasks/generate-vsphere-worker-manifest/run.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-set -ex
+set -e
 
+bosh -t "$BOSH_TARGET" login admin "$BOSH_PASSWORD"
+
+set -x
+
+BOSH_UUID=$(bosh -n --color -t "$BOSH_TARGET" status --uuid)
 FORMATTED_VSPHERE_WORKER_PRIVATE_KEY=$(echo -n "$VSPHERE_WORKER_PRIVATE_KEY" | perl -p -e 's/\n/\\\\n/g')
 
 cp lattice-ci/tasks/generate-vsphere-worker-manifest/manifest.yml .
 
+sed -i "s/BOSH-UUID/$BOSH_UUID/g" manifest.yml
 sed -i "s%TSA-PUBLIC-KEY%$CONCOURSE_TSA_PUBLIC_KEY%g" manifest.yml
 sed -i "s%WORKER-PRIVATE-KEY%\"$FORMATTED_VSPHERE_WORKER_PRIVATE_KEY\"%g" manifest.yml
 sed -i "s/VCENTER-ADDRESS/$VCENTER_ADDRESS/g" manifest.yml
