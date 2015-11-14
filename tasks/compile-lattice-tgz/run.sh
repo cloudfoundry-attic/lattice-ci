cea52cb6f8c65af072faac3e1cef0e40d766403c
Stephen Levine <slevine@pivotal.io>
2015-11-13 23:57:53 -0500

    Add lattice.tgz semver version to lattice.tgz versions directories
    [#107749884]

--- tasks/compile-lattice-tgz/run.sh
+++ tasks/compile-lattice-tgz/run.sh
@@ -2,5 +2,5 @@
 
 set -ex
 
-lattice_version=$(cat lattice-tgz-version/number)
-./lattice-release/release/build "lattice-v${lattice_version}.tgz"
+lattice_tgz_version=$(cat lattice-tgz-version/number)
+./lattice-release/release/build "lattice-v${lattice_tgz_version}.tgz" "$lattice_tgz_version"
